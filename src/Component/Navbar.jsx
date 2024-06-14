0ddae58a54228cc3393bddc24675d420cd5c601a
Vivek9988 <vivek19102004@gmail.com>
2024-06-14 14:42:32 +0530

    changes

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
       <nav className="flex justify-between bg-indigo-900 text-white py-2">
         <div className="logo">
-            <span className="font-bold text-xl mx-8">iTasl</span>
+            <span className="font-bold text-xl mx-8">To Do</span>
         </div>
      <ul className="flex gap-8 mx-9">
         <li className="cursor-pointer hover:font-bold transition-all ">Home </li>
